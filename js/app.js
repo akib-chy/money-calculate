d185eda73a761bdf24d89957666c7fe93edac2d2
Akib Chy <akibchy1212@gmail.com>
2022-02-17 12:52:53 +0600

    javascript add

--- js/app.js
+++ js/app.js
@@ -1,20 +1,33 @@
 function inputAmmount(){
     const totalInput = document.getElementById('total-balance').value;
-    const totalInputNumber = parseFloat(totalInput);
     const foodInput = document.getElementById('food-balance').value;
-    const foodInputNumber = parseFloat(foodInput);
     const rentInput = document.getElementById('rent-balance').value;
-    const rentInputNumber = parseFloat(rentInput);
     const clothInput = document.getElementById('cloth-balance').value;
-    const clothInputNumber = parseFloat(clothInput);
-    const totalItem = foodInputNumber + rentInputNumber + clothInputNumber;
-    const totalBalance = totalInputNumber - totalItem;
-    console.log(totalBalance);  
+    const alert = document.getElementById('warning');
+    const totalItem = parseFloat(foodInput) + parseFloat(rentInput) + parseFloat(clothInput);
+    const totalBalance = parseFloat(totalInput) - totalItem;
     
-    const totalExpen =  document.getElementById('total-expen');
-    totalExpen.innerText = totalItem;
-    let balance = document.getElementById('balance');
-    balance.innerText = totalBalance;
+    if(isNaN(totalInput) || isNaN(foodInput) || isNaN(rentInput) || isNaN(clothInput) || totalInput < 0 || foodInput < 0 || rentInput < 0 || clothInput < 0){
+        alert.style.display = 'block';
+    }
+    else{
+        const totalExpen =  document.getElementById('total-expen');
+        totalExpen.innerText = totalItem;
+        let balance = document.getElementById('balance');
+        balance.innerText = totalBalance;
+        alert.style.display = 'none';
+    }
+}
+function saveAmmount(){
+    const totalInput = document.getElementById('total-balance').value;
+    const saveInput = document.getElementById('save-input').value;
+    const saveAmmount = (parseFloat(totalInput) * parseFloat(saveInput)) / 100;  
+    const saveAmmountText = document.getElementById('save-ammount');
+    saveAmmountText.innerText = saveAmmount; 
+
+    const remaingAmmount =document.getElementById('remaining-ammount');
+    const balanceInput = document.getElementById('balance').innerText;
+    remaingAmmount.innerText = parseFloat(balanceInput) - parseFloat(saveAmmountText.innerText);
 }
 
 document.getElementById('calculate-btn').addEventListener('click', function(){
@@ -27,4 +40,7 @@ document.getElementById('calculate-btn').addEventListener('click', function(){
     
     console.log(totalExpences) */
     inputAmmount();
+});
+document.getElementById('btn-save').addEventListener('click', function(){
+    saveAmmount();
 })
