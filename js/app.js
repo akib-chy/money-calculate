e1d4552a64d6a656e4d06faee02556fb6f297632
Akib Chy <akibchy1212@gmail.com>
2022-02-17 16:40:47 +0600

    complate site

--- js/app.js
+++ js/app.js
@@ -1,14 +1,19 @@
+// START CALCULATE FUNCTION 
 function inputAmmount(){
+    // ALL VARIABLE
     const totalInput = document.getElementById('total-balance').value;
     const foodInput = document.getElementById('food-balance').value;
     const rentInput = document.getElementById('rent-balance').value;
     const clothInput = document.getElementById('cloth-balance').value;
-    const alert = document.getElementById('warning');
-    const totalItem = parseFloat(foodInput) + parseFloat(rentInput) + parseFloat(clothInput);
-    const totalBalance = parseFloat(totalInput) - totalItem;
     const totalExpen =  document.getElementById('total-expen');
     const balance = document.getElementById('balance');
-    
+    // POPUP WARNING
+    const alert = document.getElementById('warning');
+    // TOATAL EXPENSES AMMOUNT CALCULATE
+    const totalItem = parseFloat(foodInput) + parseFloat(rentInput) + parseFloat(clothInput);
+    // TOTAL BALANCE CALCULATE
+    const totalBalance = parseFloat(totalInput) - totalItem;
+    // CALCULATE CONDITON ADD
     if(isNaN(totalInput) || isNaN(foodInput) || isNaN(rentInput) || isNaN(clothInput) || totalInput < 0 || foodInput < 0 || rentInput < 0 || clothInput < 0){
         alert.style.display = 'block';
         totalExpen.innerText = '00';
@@ -22,15 +27,18 @@ function inputAmmount(){
         alert.style.display = 'none';
     }
 }
+// END CALCULATE FUNCTION
+// START SAVING FUNCTION
 function saveAmmount(){
+    // ALL VARIABLE
     const totalInput = document.getElementById('total-balance').value;
     const saveInput = document.getElementById('save-input').value;
     const saveAmmount = (parseFloat(totalInput) * parseFloat(saveInput)) / 100;  
     const saveAmmountText = document.getElementById('save-ammount');
     const warning1 = document.getElementById('warning2');
-
     const remaingAmmount =document.getElementById('remaining-ammount');
     const balanceInput = document.getElementById('balance').innerText;
+    // SAVING CONDITION ADD
     if(isNaN(saveInput) || saveInput < 0 && balanceInput < remaingAmmount){
         warning1.style.display = 'block'
         remaingAmmount.innerText = '00';
@@ -44,18 +52,14 @@ function saveAmmount(){
         warning1.style.display = 'none';
     }
 }
-
+// END SAVING FUNCTION
+// START CALCULATE CLICK ITEM
 document.getElementById('calculate-btn').addEventListener('click', function(){
-    /* const totalBalance = document.getElementById('total-balance').value;
-    const foodBalance = document.getElementById('food-balance').value;
-    const rentBalance = document.getElementById('rent-balance').value;
-    const clothBalance = document.getElementById('cloth-balance').value;
-   
-    const totalItem = parseFloat(foodBalance) + parseFloat(rentBalance) + parseFloat(clothBalance);
-    
-    console.log(totalExpences) */
     inputAmmount();
 });
+// END CALCULATE CLICK ITEM 
+// START SAVING CLICK ITEM 
 document.getElementById('btn-save').addEventListener('click', function(){
     saveAmmount();
-})
+});
+// END SAVING CLICK ITEM
